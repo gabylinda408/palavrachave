c52781a0f3d3523640ef9b44dbd6c4897eac6114
gabylinda408 <gabriely.semianko@escola.pr.gov.br>
2025-06-17 13:48:54 +0000

    aula 6

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    const texto = document.querySelector('#entrada-de-texto').value;
-    const campoResultado = document.querySelector('#resultado-palavrachave'); 
-    const palavras = processaTexto(texto);
-    
-  campoResultado.textContent = palavrasChave.join(', ');
+  const texto = document.querySelector("#entrada-de-texto").value;
+
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavrasChave = processaTexto(texto);
+
+  campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
 
+  const frequencias = contaFrequencias(palavras);
+
+  let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+  function ordenaPalavra(p1, p2) {
+    return frequencias[p2] - frequencias[p1];
+  }
+
+  return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
   let frequencias = {};
 
   for (let i of palavras) {
@@ -25,7 +39,5 @@ function processaTexto(texto) {
     }
   }
 
-  console.log(frequencias);
-
-  return palavras;
+  return frequencias;
 }
